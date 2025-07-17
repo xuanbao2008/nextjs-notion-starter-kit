96c3b6f8d01d1df7056b00be665c854433dbca72
“Bao <doan.bao@quoine.com>
2025-07-17 11:29:26 +0900

    update and declare type for prism

---
+++ types/prism-modules.d.ts
@@ -0,0 +1,4 @@
+declare module 'prismjs/components/prism-js-templates';
+declare module 'prismjs/components/prism-bash';
+declare module 'prismjs/components/prism-typescript';
+declare module 'prismjs/components/prism-json';
