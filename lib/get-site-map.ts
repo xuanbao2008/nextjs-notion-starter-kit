6aaa1113651e5c06bc249403e9ede75ad533686d
“Bao <doan.bao@quoine.com>
2025-07-17 17:47:25 +0900

    fix minor issue - trying

--- lib/get-site-map.ts
+++ lib/get-site-map.ts
@@ -62,21 +62,20 @@ async function getAllPagesImpl(
     if (!recordMap) continue
 
     const block = recordMap.block[pageId]?.value
-    if (
-      !(getPageProperty<boolean | null>('Public', block!, recordMap) ?? true)
-    ) {
-      continue
-    }
-
+    if (!block) continue
+  
+    const isPublic = getPageProperty<boolean | null>('Public', block, recordMap) ?? true
+    if (!isPublic) continue
+  
     const canonicalPageId = getCanonicalPageId(pageId, recordMap, { uuid })!
     const slugProp = getPageProperty<string>('Slug', block, recordMap)
     const title = getPageProperty<string>('title', block, recordMap)
     const categoryProp = getPageProperty<string>('Category', block, recordMap)
-
+  
     const slug = toSlug(slugProp || title)
     const category = toSlug(categoryProp || '')
     const fullSlug = category ? `${category}/${slug}` : slug
-
+  
     if (canonicalPageMap[fullSlug]) {
       console.warn('warning duplicate canonical page id (slug)', {
         fullSlug,
@@ -85,9 +84,9 @@ async function getAllPagesImpl(
       })
       continue
     }
-
+  
     canonicalPageMap[fullSlug] = pageId
-  }
+  }  
 
   return {
     pageMap,
