84aca7f3a520e677c88ee3b51614aeb6edd505a3
“Bao <doan.bao@quoine.com>
2025-07-26 00:07:04 +0900

    lint vietnamese slug script

--- lib/normalize-title.ts
+++ lib/normalize-title.ts
@@ -1,29 +1,26 @@
-export const normalizeTitle = (str?: string | null): string => {
-  if (!str) return ''
+export const normalizeTitle = (title?: string | null): string => {
+  const input = (title || '').toLowerCase()
 
-  const a =
-    'àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;'
-  const b =
-    'aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------'
-  const map = new RegExp(a.split('').join('|'), 'g')
+  return input
+    // Remove Vietnamese accents
+    .replaceAll(/[áàảạãăắằẳẵặâấầẩẫậ]/g, 'a')
+    .replaceAll(/[éèẻẽẹêếềểễệ]/g, 'e')
+    .replaceAll(/[íìỉĩị]/g, 'i')
+    .replaceAll(/[óòỏõọôốồổỗộơớờởỡợ]/g, 'o')
+    .replaceAll(/[úùủũụưứừửữự]/g, 'u')
+    .replaceAll(/[ýỳỷỹỵ]/g, 'y')
+    .replaceAll(/đ/g, 'd')
 
-  return str
-    .toString()
-    .toLowerCase()
-    // Vietnamese-specific replacements
-    .replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a')
-    .replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e')
-    .replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i')
-    .replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o')
-    .replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u')
-    .replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y')
-    .replace(/đ/gi, 'd')
-    // General slug cleanup
-    .replace(/\s+/g, '-') // replace spaces with -
-    .replace(map, c => b.charAt(a.indexOf(c))) // map accented chars
-    .replace(/&/g, '-and-') // replace & with 'and'
-    .replace(/[^\w\-]+/g, '') // remove all non-word chars
-    .replace(/\-\-+/g, '-') // replace multiple - with single -
-    .replace(/^-+/, '') // trim - from start
-    .replace(/-+$/, '') // trim - from end
+    // Replace whitespace and some symbols with hyphens
+    .replaceAll(/[\s·/_,:;]+/g, '-')
+
+    // Remove all remaining non-word characters except hyphens
+    .replaceAll(/[^a-z0-9-]/g, '')
+
+    // Collapse multiple hyphens
+    .replaceAll(/--+/g, '-')
+
+    // Trim hyphens from the start and end (using `.replace`)
+    .replace(/^-+/, '')
+    .replace(/-+$/, '')
 }
